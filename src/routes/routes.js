f49106eb7d24b38e190bcbb9014955bc2938c485
Fran Bejarano Escano <147075385+FranEscano@users.noreply.github.com>
2023-12-05 11:18:57 +0000

    first commit

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,8 +3,8 @@ const {body} = require('express-validator')
 const controller = require('../controllers/controllers.js')
 const router = new Router()
 
-router.get('/init', controller.initDatabase)
-router.get('/', controller.getSubscribers)
+router.get('/', controller.initDatabase)
+router.get('/list', controller.getSubscribers)
 router.post('/subscribe',
     body('email').isEmail().normalizeEmail(),
     body('firstname').not().isEmpty().escape(),
